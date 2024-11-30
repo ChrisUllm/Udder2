0111be5128b47f28c6ae7a5576cd943755ce37d5
ChrisUllm <culleweit@hotmail.com>
2024-11-30 10:51:42 +0100

    Image added

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
             Text("Best app ever!")
             Text("Trust me!")
             Text("Trust me!!!")
+            Image(/*@START_MENU_TOKEN@*/"Landscapes"/*@END_MENU_TOKEN@*/)
+                .resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
